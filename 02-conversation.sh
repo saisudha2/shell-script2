e4d44bbaf856961ebf0e58e9343346eda65c9c05
saisudha22 <ankesaisudha@gmail.com>
2025-08-11 16:28:38 +0530

    i am practicing shell

---
+++ 02-conversation.sh
@@ -0,0 +1,7 @@
+#1/bin/bash
+
+echo "Ram:: Hey Krishna, How are you?"
+echo "Krishna:: Hello Ram, I am fine. How are you doing"
+echo "Ram:: I am fine too. what's up?"
+echo "krishna:: Nothing going to office now,will you come?"
+echo "Ram:: sorry, you carry on! I will come once my work compeleted"
