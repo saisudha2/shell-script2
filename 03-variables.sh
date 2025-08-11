2532cce52c25becea6c702dae13a44a0676c276b
saisudha22 <ankesaisudha@gmail.com>
2025-08-11 16:37:25 +0530

    i am practicing shell

---
+++ 03-variables.sh
@@ -0,0 +1,10 @@
+#1/bin/bash
+
+PERSON1=Ram
+PERSON2=Krishna
+
+echo "$PERSON1:: Hey $PERSON2, How are you?"
+echo "$PERSON2:: Hello $PERSON1, I am fine. How are you doing"
+echo "$PERSON1:: I am fine too. what's up?"
+echo "$PERSON2:: Nothing going to office now,will you come?"
+echo "$PERSON1:: sorry, you carry on! I will come once my work compeleted"
