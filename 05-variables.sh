969a0ec028ad62f11309480bbadec62ad9644d6c
saisudha22 <ankesaisudha@gmail.com>
2025-08-11 18:30:48 +0530

    i am practicing shell

---
+++ 05-variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "please enter your username:: "
+
+read -s USERNAME
+
+echo "please enter password:: "
+
+read -s PASSWORD
+
+echo "Username is: $USERNAME, Password is: $Password"
