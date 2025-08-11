b8ed3f74d04f62da24b31e1de743513016741c83
saisudha22 <ankesaisudha@gmail.com>
2025-08-11 18:45:46 +0530

    i am practicing shell

---
+++ 06-datatypes.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+NUMBER1=$1
+NUMBER2=$2
+
+SUM=$(($NUMBER1+$NUMBER2))
+
+echo "total:: $SUM"
+
+echo "how many args passed:: $#"
+
+echo "All args passed:: $@"
+
+echo "script name:: $0"
