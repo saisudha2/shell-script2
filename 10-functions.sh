f339e3061813984d51bce6cdf854422f426f9696
saisudha22 <ankesaisudha@gmail.com>
2025-08-12 18:20:52 +0530

    i am practicing shellscript

---
+++ 10-functions.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+ID=$(id -u)
+
+VALIDATE(){
+ if [ $1 -ne 0 ]
+ then 
+   echo "ERROR:: $2 ...FAILED"
+   exit 1
+ else
+   echo "$2 .... SUCCESS"
+  fi 
+}
+
+if [ $ID -ne 0 ]
+then
+  echo "ERROR:: please run this script with root access"
+  exit 1
+else
+   echo "you are root user"
+fi
+
+ yum install mysql -y
+
+ VALIDATE $? "installing mysql"
+
+ yum install git -y 
+
+VALIDATE $? "installing git"
+
