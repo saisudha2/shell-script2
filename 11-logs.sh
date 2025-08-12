d2061b427fa448a569268f377a78ca8158202a27
saisudha22 <ankesaisudha@gmail.com>
2025-08-12 18:54:05 +0530

    i am practicing shellscript

---
+++ 11-logs.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+ID=$(id -u)
+
+TIMESTAMP=$(date +%F-%H-%M-%S)
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+
+echo "Script started executing at $TIMESTAMP" &>> $LOGFILE
+
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+      echo -e " ERROR:: $2.. is $R Failed $N"
+      exit 1
+    else
+      echo -e "$2 is .... $G SUCCESS $N"
+    fi  
+}
+
+if [ $ID -ne 0 ]
+then
+  echo -e "$R ERROR:: please run this script with root access $N"
+  exit 1
+else
+   echo " $G you are root user $N"
+fi
+
+yum install mysql -y &>> $LOGFILE
+
+VALIDATE $? "Installing MySQL"
+ 
+yum install git -y &>> $LOGFILE
+
+VALIDATE $? "Installing Git"
